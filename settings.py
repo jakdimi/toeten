6a18cb2ba0c67fa061e9b834e2b2598a1a80e6f3
jakdimi <47759595+jakdimi@users.noreply.github.com>
2022-09-22 16:23:21 +0200

    changed port

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-PORT = 10000
+PORT = 5000
 INITIAL_PLAYERS = [
     'David', 'Felix', 'Hannah', 'Jakob B.', 'Jakob D.', 'Judith', 'Lara',
     'Leonie', 'Lisa', 'Sanne', 'Soeren', 'Sophia', 'Steffen', 'Timo'
